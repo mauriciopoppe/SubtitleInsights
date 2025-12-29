1c4b97d463f34cfff358bc711c4a04331be1f5a0
Mauricio Poppe <mauricio.poppe@gmail.com>
2025-12-28 19:48:53 -0500

    conductor(checkpoint): Checkpoint end of Phase 1 (State Management Updates)

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,18 +1,36 @@
 export class Config {
-  private static STORAGE_KEY = "lle_is_enabled";
+  private static MASTER_STORAGE_KEY = "lle_is_enabled";
+  private static OVERLAY_STORAGE_KEY = "lle_is_overlay_enabled";
 
   static async getIsEnabled(): Promise<boolean> {
     return new Promise((resolve) => {
-      chrome.storage.local.get([this.STORAGE_KEY], (result) => {
+      chrome.storage.local.get([this.MASTER_STORAGE_KEY], (result) => {
         // Default to true if not set
-        resolve(result[this.STORAGE_KEY] ?? true);
+        resolve(result[this.MASTER_STORAGE_KEY] ?? true);
       });
     });
   }
 
   static async setIsEnabled(value: boolean): Promise<void> {
     return new Promise((resolve) => {
-      chrome.storage.local.set({ [this.STORAGE_KEY]: value }, () => {
+      chrome.storage.local.set({ [this.MASTER_STORAGE_KEY]: value }, () => {
+        resolve();
+      });
+    });
+  }
+
+  static async getIsOverlayEnabled(): Promise<boolean> {
+    return new Promise((resolve) => {
+      chrome.storage.local.get([this.OVERLAY_STORAGE_KEY], (result) => {
+        // Default to true if not set
+        resolve(result[this.OVERLAY_STORAGE_KEY] ?? true);
+      });
+    });
+  }
+
+  static async setIsOverlayEnabled(value: boolean): Promise<void> {
+    return new Promise((resolve) => {
+      chrome.storage.local.set({ [this.OVERLAY_STORAGE_KEY]: value }, () => {
         resolve();
       });
     });
@@ -20,8 +38,16 @@ export class Config {
 
   static addChangeListener(callback: (isEnabled: boolean) => void) {
     chrome.storage.onChanged.addListener((changes, areaName) => {
-      if (areaName === "local" && changes[this.STORAGE_KEY]) {
-        callback(changes[this.STORAGE_KEY].newValue);
+      if (areaName === "local" && changes[this.MASTER_STORAGE_KEY]) {
+        callback(changes[this.MASTER_STORAGE_KEY].newValue);
+      }
+    });
+  }
+
+  static addOverlayChangeListener(callback: (isOverlayEnabled: boolean) => void) {
+    chrome.storage.onChanged.addListener((changes, areaName) => {
+      if (areaName === "local" && changes[this.OVERLAY_STORAGE_KEY]) {
+        callback(changes[this.OVERLAY_STORAGE_KEY].newValue);
       }
     });
   }
